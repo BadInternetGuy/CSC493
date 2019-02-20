fe40752af0c609f4e700256e8b9ded6ce0b36093
BadInternetGuy <BadInternetGuy@users.noreply.github.com>
2019-02-20 16:02:00 -0500

    Some more basic changes to the site. Adds carousel.

--- app.py
+++ app.py
@@ -21,6 +21,30 @@ def accountCreated():
 def forgotPassword():
 	return render_template("forgotPassword.html")
 
+@app.route("/testPage")
+def testPage():
+    return render_template("testPage.html")
+
+
+@app.route("/anthemHome")
+def anthemHome():
+    return render_template("anthemHome.html")
+
+@app.route("/leagueHome")
+def leagueHome():
+    return render_template("leagueHome.html")
+
+@app.route("/destinyHome")
+def destinyHome():
+    return render_template("destinyHome.html")
+
+@app.route("/hollowKnightHome")
+def hollowKnightHome():
+    return render_template("hollowKnightHome.html")
+
+@app.route("/mtgHome")
+def mtghome():
+    return render_template("mtgHome.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
