4c21d080defee01758982f60e051037d6a1dbc40
BadInternetGuy <youngdu@berea.edu>
2019-04-17 16:55:03 -0400

    Adds some more changes. Still experimenting with database.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, request
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, redirect, url_for, request, session
+import MySQLdb
 
 
 #def connect():
@@ -8,16 +8,18 @@ from flask_mysqldb import MySQL
 #    cur = conn.cursor()
 #    return conn, cur
 
+def connect():
+    conn = MySQLdb.connect(db="GameChat", host="localhost", user="root", passwd="Dustin2018")
 
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Dustin2018'
-app.config['MYSQL_DB'] = 'GameChat'
+#app.config['MYSQL_HOST'] = 'localhost'
+#app.config['MYSQL_USER'] = 'root'
+#app.config['MYSQL_PASSWORD'] = 'Dustin2018'
+#app.config['MYSQL_DB'] = 'GameChat'
 
-mysql = MySQL(app)
+#mysql = MySQL(app)
 
 
 
@@ -25,19 +27,21 @@ mysql = MySQL(app)
 def landingpad():
     return render_template("landingpad.html")
 
-@app.route("/createAccount", methods=['GET, POST'])
+@app.route("/createAccount", methods=['GET', 'POST'])
 def createAccount():
-    if request.method =="POST":
-        details = request.form
-        username = details['username']
-        password = details['password']
-        email = details['email']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO USERS(username, password, email) VALUES (%s, %s, %s)", (username, password, email))
-        mysql.connection.commit()
-        cur.close()
-        return 'success'
-	return render_template("createAccount.html",)
+    conn, cur = connect()
+
+    data={}
+    data['email'] = request.form.get('email')
+    data['password'] = request.form.get('password')
+    data['username'] = request.form.get('username')
+
+    query = "INSERT INTO `USERS` (`email`, `password`, `username`) VALUES ('{0}', '{1}', '{2}')".format(data['email'],data['password'],data['username'])
+    cur.execute(query)
+    conn.commit()
+    conn.close()
+
+    return render_template("accountCreated.html")
 
 @app.route("/welcome")
 def welcome():
@@ -50,11 +54,6 @@ def accountCreated():
 @app.route("/forgotPassword")
 def forgotPassword():
 	return render_template("forgotPassword.html")
-
-@app.route("/testPage")
-def testPage():
-    return render_template("testPage.html")
-
 
 @app.route("/anthemHome")
 def anthemHome():
