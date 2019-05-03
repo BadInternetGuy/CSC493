5b8d15f686118750fba96d6d0f496d4bec975d9e
BadInternetGuy <youngdu@berea.edu>
2019-05-02 20:19:17 -0400

    Final changes

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
+"""
+This is the code for GameChat.
+Created by: Dustin Young
+
+Special thanks:
+
+Richard Maiti
+Scott Heggen
+Mario Nakazawa
+Jan Pearce
+"""
+
 from flask import Flask, render_template, redirect, url_for, request, session
 import MySQLdb
 import mysql.connector
 
 
-#def connect():
 
-#    conn = MySQLdb.connect(db="GameChat", host="localhost", user="root", passwd="Dustin2018", port=3306)
-#    cur = conn.cursor()
-#    return conn, cur
-
-#def connect():
-    #conn = MySQLdb.connect(db="GameChat", host="localhost", user="root", passwd="Dustin2018")
+# Establish connection to the database.
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -22,19 +28,14 @@ mydb = mysql.connector.connect(
 
 app = Flask(__name__)
 
-#app.config['MYSQL_HOST'] = 'localhost'
-#app.config['MYSQL_USER'] = 'root'
-#app.config['MYSQL_PASSWORD'] = 'Dustin2018'
-#app.config['MYSQL_DB'] = 'GameChat'
-
-#mysql = MySQL(app)
-
 
 
 @app.route("/")
 def landingpad():
     return render_template("landingpad.html")
 
+
+#Allows the creation of an account.
 @app.route("/createAccount", methods=['GET', 'POST'])
 def createAccount():
     mycursor = mydb.cursor()
@@ -85,6 +86,8 @@ def hollowKnightHome():
 @app.route("/mtgHome")
 def mtghome():
     return render_template("mtgHome.html")
+
+    #Set to debug mode to enable auto-refresh 
 
 if __name__ == '__main__':
     app.run(debug=True)
